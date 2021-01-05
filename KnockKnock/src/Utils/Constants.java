aeb36e146b3d9f6bff6748055422681c88092144
Vo1z <vitya.voody@gmail.com>
2021-01-05 17:52:55 +0200

    PortKnockingAuthentication 0.1.5
    -AuthenticationClient now can work in two modes(INFINITE_LIFETIME and TIMEOUT_LIFETIME) (tested)
    -Project structure refactoring

--- KnockKnock/src/Utils/Constants.java
+++ KnockKnock/src/Utils/Constants.java
@@ -8,10 +8,11 @@ public class Constants
     public static final int MAX_DATAGRAM_SIZE = MIN_MTU - MAX_IP_HEADER_SIZE - UDP_HEADER_SIZE;
     public static final int SERVER_SOCKET_CONNECTION_TIMEOUT = 0;
     public static final int AUTHENTICATION_SOCKET_TIMEOUT = 0;
+    public static final int CLIENT_SOCKET_TIMEOUT = 10000;
     public static final int MESSAGE_DELAY_MILLISECONDS = 30;
     public static final String PORT_REGEX = ":\\d+|/";
     public static final String ADDRESS_REGEX = "(\\d+(\\.|:)+)|/";
     public static final String ADDRESS_REGEX_ON_ARGUMENTS = "(\\d+\\.)+(\\d+)?";
     public static boolean IS_RUNTIME_IN_DEBUG_MODE = false;
-
+    public static boolean IS_CLIENT_HAS_INFINITE_LIFETIME = false;
 }
